f350a93a9277fbf6350aa2786614b6955dd28dfd
danusorn23456 <danusorn23456@gmail.com>
2023-07-26 08:38:43 +0700

    change the way to draw block with utils

--- src/component/model/index.tsx
+++ src/component/model/index.tsx
@@ -1,70 +1,74 @@
 import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
-import { BoxGeometry, Group, Mesh, MeshStandardMaterial } from "three";
-import { McModel, McModelMeshProps, McSimplifyModel } from "~/types";
-import { getModelMeshProps } from "~/utils";
+import {
+  BoxGeometry,
+  Group,
+  MathUtils,
+  Mesh,
+  MeshStandardMaterial,
+} from "three";
+import { McModel } from "~/types";
+import { McUtils } from "~/utils";
 
 export interface Model {
-  model: McSimplifyModel;
+  model: McModel;
   spin?: boolean;
+  bounding?: boolean;
 }
 
-const Model = ({ model, spin }: Model) => {
+const Model = ({ model, spin, bounding }: Model) => {
   const groupRef = useRef<Group>(null);
 
   useEffect(() => {
-    function recursiveGetModelMeshProps(
-      model: McSimplifyModel | McModel,
-      prev?: McModelMeshProps[]
-    ): McModelMeshProps[] {
-      let returnValue = [...(prev || [])];
+    function recursiveDrawBlock(model: McModel) {
+      if (model?.from && model?.to) {
+        const args = McUtils.fromToToCoordinates(model.from, model.to);
+        const position = McUtils.fromToToPosition(model.from, model.to);
 
-      if (model?.elements) {
+        const geometry = new BoxGeometry(...args);
+        const material = new MeshStandardMaterial({ transparent: true });
+        const mesh = new Mesh(geometry, material);
+
+        mesh.position.set(...position);
+
+        if (model.rotation) {
+          mesh.rotation[model.rotation.axis as "x" | "y" | "z"] =
+            -MathUtils.degToRad(model.rotation.angle);
+        }
+
+        material.color.set("#242424");
+
+        groupRef.current?.add(mesh);
+      }
+
+      if (model.elements) {
         for (const key in model.elements) {
-          const currentModal = model.elements[key];
-          returnValue.push(
-            getModelMeshProps(
-              currentModal,
-              currentModal.model || currentModal.__comment
-            )
-          );
+          const currentModel = model.elements[key];
+          recursiveDrawBlock(currentModel);
         }
       }
 
       if (model?.parent instanceof Object) {
-        return recursiveGetModelMeshProps(model.parent, returnValue);
+        recursiveDrawBlock(model.parent);
+        if (model.parent.elements) {
+          for (const key in model.parent.elements) {
+            const currentModel = model.parent.elements[key];
+            recursiveDrawBlock(currentModel);
+          }
+        }
       }
-
-      return returnValue;
     }
 
-    const modelMeshs = recursiveGetModelMeshProps(model);
+    recursiveDrawBlock(model);
 
-    for (let index = 0; index < modelMeshs.length; index++) {
-      const {
-        geometryProps: { args = [16, 16, 16] },
-        meshProps: { name = "", position, rotation },
-      } = modelMeshs[index];
-
-      const isBounding =
-        args.every((v) => v === args[0]) && modelMeshs.length > 1;
-
-      const geometry = new BoxGeometry(...args);
-      const material = new MeshStandardMaterial({ transparent: true });
+    if (bounding) {
+      const geometry = new BoxGeometry(16, 16, 16);
+      const material = new MeshStandardMaterial({
+        color: "#ff0000",
+        wireframe: true,
+      });
       const mesh = new Mesh(geometry, material);
-
-      material.color.set("#242424");
-
-      if (isBounding) {
-        material.wireframe = true;
-      } else {
-        mesh.name = name;
-        mesh.rotation.set(...rotation.value);
-        mesh.position.set(...position);
-      }
-
       groupRef.current?.add(mesh);
-      groupRef.current?.updateMatrix();
     }
 
     if (spin)
